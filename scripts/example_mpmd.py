ad0073f3dcd979db69997d442d920684d58df471
Yaoshiang Ho <yho@google.com>
2025-12-04 05:51:17 +0000

    Fixes

--- scripts/example_mpmd.py
+++ scripts/example_mpmd.py
@@ -1,6 +1,6 @@
 """Proof-of-concept that PyTorch does support limited MPMD already.
 
-The "cluster" in question is composed of two devices: The CUDA GPU and the CPU. 
+The "cluster" in question is composed of two devices: The CUDA GPU and the CPU.
 
 We use a linear regression as the stand-in for a complex model. The
 inference server runs on cuda because pytorch cuda supports async ops. We add
@@ -8,107 +8,11 @@ a cuda-sleep op to simulate a small, slow inference cluster.
 
 The training server runs on CPU and does a much heavier weight computation.
 
-Note that the rollout server runs at a different pace than the training server. 
+Note that the rollout server runs at a different pace than the training server.
 We find empirically that we have a replay ratio of about 9:1. Each
-rollout is re-used about 9 times. 
+rollout is re-used about 9 times.
 
 ```
-(tt) yho_google_com@yho-l4:~/Documents/GitHub/sandbox$ python scripts/example_mpmd.py
-
-rollout_step=0
-
-rollout_step=1
-  train_step=0, loss.item()=0.7277
-  train_step=1, loss.item()=0.7257
-  train_step=2, loss.item()=0.7304
-
-rollout_step=2
-  train_step=0, loss.item()=0.0471
-  train_step=1, loss.item()=0.0534
-  train_step=2, loss.item()=0.0444
-  train_step=3, loss.item()=0.0415
-  train_step=4, loss.item()=0.0517
-  train_step=5, loss.item()=0.0443
-  train_step=6, loss.item()=0.0531
-  train_step=7, loss.item()=0.0510
-  train_step=8, loss.item()=0.0405
-
-rollout_step=3
-  train_step=0, loss.item()=0.0489
-  train_step=1, loss.item()=0.0475
-  train_step=2, loss.item()=0.0567
-  train_step=3, loss.item()=0.0472
-  train_step=4, loss.item()=0.0443
-  train_step=5, loss.item()=0.0552
-  train_step=6, loss.item()=0.0489
-  train_step=7, loss.item()=0.0517
-  train_step=8, loss.item()=0.0398
-
-rollout_step=4
-  train_step=0, loss.item()=0.0482
-  train_step=1, loss.item()=0.0457
-  train_step=2, loss.item()=0.0448
-  train_step=3, loss.item()=0.0467
-  train_step=4, loss.item()=0.0543
-  train_step=5, loss.item()=0.0547
-  train_step=6, loss.item()=0.0488
-  train_step=7, loss.item()=0.0484
-  train_step=8, loss.item()=0.0431
-
-rollout_step=5
-  train_step=0, loss.item()=0.0520
-  train_step=1, loss.item()=0.0539
-  train_step=2, loss.item()=0.0551
-  train_step=3, loss.item()=0.0536
-  train_step=4, loss.item()=0.0448
-  train_step=5, loss.item()=0.0561
-  train_step=6, loss.item()=0.0429
-  train_step=7, loss.item()=0.0489
-  train_step=8, loss.item()=0.0562
-
-rollout_step=6
-  train_step=0, loss.item()=0.0512
-  train_step=1, loss.item()=0.0381
-  train_step=2, loss.item()=0.0466
-  train_step=3, loss.item()=0.0413
-  train_step=4, loss.item()=0.0475
-  train_step=5, loss.item()=0.0379
-  train_step=6, loss.item()=0.0540
-  train_step=7, loss.item()=0.0435
-  train_step=8, loss.item()=0.0530
-
-rollout_step=7
-  train_step=0, loss.item()=0.0501
-  train_step=1, loss.item()=0.0434
-  train_step=2, loss.item()=0.0464
-  train_step=3, loss.item()=0.0473
-  train_step=4, loss.item()=0.0478
-  train_step=5, loss.item()=0.0422
-  train_step=6, loss.item()=0.0592
-  train_step=7, loss.item()=0.0468
-  train_step=8, loss.item()=0.0491
-
-rollout_step=8
-  train_step=0, loss.item()=0.0444
-  train_step=1, loss.item()=0.0514
-  train_step=2, loss.item()=0.0518
-  train_step=3, loss.item()=0.0490
-  train_step=4, loss.item()=0.0563
-  train_step=5, loss.item()=0.0561
-  train_step=6, loss.item()=0.0451
-  train_step=7, loss.item()=0.0514
-  train_step=8, loss.item()=0.0544
-
-rollout_step=9
-  train_step=0, loss.item()=0.0526
-  train_step=1, loss.item()=0.0474
-  train_step=2, loss.item()=0.0436
-  train_step=3, loss.item()=0.0475
-  train_step=4, loss.item()=0.0532
-  train_step=5, loss.item()=0.0484
-  train_step=6, loss.item()=0.0527
-  train_step=7, loss.item()=0.0582
-  train_step=8, loss.item()=0.0603
 ```
 """
 
@@ -122,7 +26,7 @@ OUTPUT_DIM = 1
 torch.set_default_dtype(torch.float64)
 
 
-def rl_inference_rollout(model, prompt, device):
+def rl_inference_rollout(model, prompt):
     """Runs one step of inference on a small, slow cluster.
 
     Args:
@@ -132,28 +36,28 @@ def rl_inference_rollout(model, prompt, device):
     Returns:
         A scalar representing the "completion", on CPU
     """
-    model.to(device)
+    model.eval()
     rollout = model(prompt)
+    # Simulate sampling by fuzzing the "rollout"
+    rollout = rollout + (torch.rand_like(rollout, requires_grad=False) * 0.1)
+    # Simulate a "small, slow" cluster by sleeping on CUDA
+    torch.cuda._sleep(5 * 10**6)
     return rollout
 
 
-def rl_training_step(model, prompt, rollout, criterion, optimizer, device):
+def rl_training_step(model, prompt, rollout, criterion, optimizer):
     """Runs one step of training on a "larger, faster" cluster.
 
     Returns:
         A scalar loss.
     """
-    model.eval()
-    rollout = rollout
+    model.train()
     prediction = model(prompt)
     loss = criterion(prediction, rollout)
-    # Simulate a RL loss by fuzzing the loss
-    loss = loss + (torch.rand_like(loss, requires_grad=False) * 0.1)
-    loss = loss.mean()
     loss.backward()
 
-    optimizer.zero_grad()
     optimizer.step()
+    optimizer.zero_grad()
     return loss
 
 
@@ -164,7 +68,7 @@ def main():
 
     # Create optimizer and loss
     optimizer = torch.optim.SGD(model_training.parameters(), lr=1e-3)
-    criterion = torch.nn.MSELoss(reduction="none")
+    criterion = torch.nn.MSELoss()
 
     replay_buffer = None
 
@@ -178,16 +82,17 @@ def main():
 
         with torch.inference_mode():
             rollout = rl_inference_rollout(model_inference, prompt.to("cuda"))
-            torch.cuda._sleep(10**7)
             done_event.record()
 
         if rollout_step == 0:
             done_event.synchronize()  # Ensure first step is complete and the replay buffer has something
+            replay_buffer = (prompt.cpu(), rollout.cpu())
+            continue
 
+        # On rollout_step > 0, we have a replay buffer
         # Poll until rollout is ready
         for train_step in range(50):
             if done_event.query():
-                # Always breaks on rollout_step==0
                 break
             loss = rl_training_step(
                 model_training,
