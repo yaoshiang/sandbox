7e10652171f295c422e071fda2748a86d707e5ac
Yaoshiang Ho <yaoshiang@gmail.com>
2026-04-13 23:34:23 +0000

    added LLO dumps and tpu device to excess precision example.

--- scripts/jax_excess_precision.py
+++ scripts/jax_excess_precision.py
@@ -5,7 +5,7 @@ The first algorithm is the "eager" mode with proper downcasts to f16.
 The second algorithm compiles. Run with different flags to see different results.
 
 ```
-$ XLA_FLAGS="--xla_allow_excess_precision=true" python scripts/jax_excess_precision.py
+$ LIBTPU_INIT_ARGS="--xla_jf_dump_llo_text=true --xla_jf_dump_to=/tmp/llodump" XLA_FLAGS="--xla_allow_excess_precision=true" python scripts/jax_excess_precision.py
 
 [[1.015625 0.      ]
  [0.       1.015625]] float32
@@ -16,7 +16,7 @@ $ XLA_FLAGS="--xla_allow_excess_precision=true" python scripts/jax_excess_precis
 and
 
 ```
-$ XLA_FLAGS="--xla_allow_excess_precision=false" python scripts/jax_excess_precision.py
+$ LIBTPU_INIT_ARGS="--xla_jf_dump_llo_text=true --xla_jf_dump_to=/tmp/llodump" XLA_FLAGS="--xla_allow_excess_precision=false" python scripts/jax_excess_precision.py
 
 [[1.015625 0.      ]
  [0.       1.015625]] float32
@@ -37,8 +37,11 @@ def main():
 
     finfo = jnp.finfo("bfloat16")
     x = (jnp.bfloat16(1) + finfo.eps) * jnp.eye(2, dtype="bfloat16")
-    print(f(x), f(x).dtype)
-    print(jax.jit(f)(x), jax.jit(f)(x).dtype)
+    x = jax.device_put(x, jax.local_devices()[0])
+    print("EAGER:\n", f(x), f(x).dtype)
+    print("COMPILED:\n", jax.jit(f)(x), jax.jit(f)(x).dtype)
+
+    print(jax.jit(f).trace(x).lower().compile().as_text())
 
 
 if __name__ == "__main__":
